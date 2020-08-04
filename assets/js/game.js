c0ffef809633fcfb3f55e87344a26d56bb7ab03c
RonanSilasSmith <smith.ronan@outlook.com>
2020-08-04 02:45:34 -0800

    adds combat

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,38 @@
-function fight(){
-    window.alert("the fight has begun!");
+var playerName = window.prompt("What is you characters name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+
+var fight = function() {
+    window.alert("welcome to Robot Gladiators");
+
+    enemyHealth = enemyHealth - playerAttack;
+
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health."
+    )
+
+if (enemyHealth  <= 0){
+    window.alert(enemyName + " has died!")
+}else {
+    window.alert(enemyName + "still has " + enemyHealth + " health left")
 }
 
 
-var playerName = window.prompt("What is you characters name?");
+    playerHealth = playerHealth - enemyAttack;
 
-//fight();
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health."
+    )
 
-console.log(playerName)
+
+
+}
+
+fight();
